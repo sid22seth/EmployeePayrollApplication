81b204b65672fe0d4d9da650b35eee3afbbb8632
Administrator <siddhiseth3009@gmail.com>
2020-11-24 15:18:08 +0530

    [Siddhi]Add. employeePayrollDetails added from local storage

--- js/home.js
+++ js/home.js
@@ -1,10 +1,16 @@
+let employeePayrollList;
 window.addEventListener("DOMContentLoaded", (event) => {
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
 });
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList')?JSON.parse(localStorage.getItem('EmployeePayrollList')):[];
+}
 const createInnerHtml = () => {
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
+    if(employeePayrollList.length == 0) return;
     let innerHtml = `${headerHtml}`;
-    let employeePayrollList = createEmployeePayrollJSON();
     for(const employeePayrollData of employeePayrollList){
         innerHtml =`${innerHtml}
         <tr>
