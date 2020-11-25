de8e5b0492a2db923c5e0b365b2cc1a79afd1312
Administrator <siddhiseth3009@gmail.com>
2020-11-25 21:28:39 +0530

    [Siddhi]Add. Promise.js added

--- Service/AjaxCallback.js
+++ Service/AjaxCallback.js
@@ -29,7 +29,7 @@ function makeAJAXCall(methodType, url, callback, async=true, data=null){
     else xhr.send();
     console.log(methodType+" Request sent to server");
 }
-const getUrl = " http://localhost:3000/employees/"
+const getUrl = " http://localhost:3000/employees/1"
 function getUserDetails(data)
 {
     console.log("Get User Data at: " + showTime() + " Value: " + data);
@@ -40,7 +40,7 @@ console.log("Made GET AJAX call to the server at " + showTime());
 const deleteURL = "http://localhost:3000/employees/13";
 function userDeletedData(data)
 {
-    console.log("User Deleted : "+ data);
+    console.log("User Deleted at: " + showTime() + "Value: "+ data);
 }
 makeAJAXCall("DELETE", deleteURL, userDeletedData, false);
 console.log("Made DELETE AJAX call to the server at " + showTime());
@@ -49,7 +49,7 @@ const postURL = "http://localhost:3000/employees";
 const empData = {"name":"Siddhi","salary":"90000"};
 function userAdded(data)
 {
-    console.log("User Added : "+ data);
+    console.log("User Added at: " + showTime() + "Value: "+ data);
 }
 makeAJAXCall("POST", postURL, userAdded, true, empData);
 console.log("Made POST AJAX call to the server at " + showTime());
---
+++ Service/Promise.js
@@ -0,0 +1,62 @@
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+function showTime()
+{
+    const date = new Date();
+    return date.getHours() + "Hrs: " + date.getMinutes() + "Mins: " + date.getSeconds() + "Secs:";
+}
+function makePromiseCall(methodType, url, async=true, data=null)
+{
+    return new Promise(function (resolve, reject)
+    {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function()
+        {
+            //console.log(methodType + " State Changed called at: " + showTime() + "Ready state: " + xhr.readyState + " Status: " + xhr.status)
+            if(xhr.status.toString().match("^[2][0-9]{2}$"))
+            {
+                    resolve(xhr.responseText);
+            }
+            else if(xhr.status.toString().match("^[4,5][0-9]{2}$"))
+            {
+                reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+                }); 
+                console.log("Handle 400 client error or 500 server error");
+            }
+        }
+        xhr.open(methodType, url, async);
+        if(data)
+        {
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        }
+        else xhr.send();
+        console.log(methodType+" Request sent to server");
+    });
+}
+
+const getURL = "http://localhost:3000/employees/1";
+makePromiseCall("GET", getURL, true)
+               .then(responseText => {
+                console.log("Get User Data at: " + showTime() + " Value: " + responseText);
+            })
+               .catch(error => console.log("Get error status : "+ JSON.stringify(error)));
+console.log("Made GET Promise call to the server at " + showTime());
+
+const deleteURL = "http://localhost:3000/employees/13";
+makePromiseCall("DELETE", deleteURL, false)
+            .then(responseText => {
+                console.log("User Deleted at: " + showTime() + "Value: "+ responseText);
+            })
+            .catch(error => console.log("delete error status : "+ JSON.stringify(error)));
+console.log("Made DELETE Promise call to the server at " + showTime());
+
+const postURL = "http://localhost:3000/employees";
+const empData = {"name":"Siddhi","salary":"90000"};
+makePromiseCall("POST", postURL, true, empData)
+               .then(responseText => {
+                console.log("User Added at: " + showTime() + "Value: "+ responseText);
+            })
+            .catch(error => console.log("post error status : "+ JSON.stringify(error)));
+console.log("Made POST Promise call to the server at " + showTime());
